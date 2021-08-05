a09ce344494e18eeffa80fa4d25e0e0e30d3bc5a
Tomi <tomi.taskila@tyomaa.com>
2021-08-05 16:46:41 +0300

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs = []) => {
+  const likes = blogs.reduce((total, blog) => {
+    return total + (blog.likes || 0)
+  }, 0)
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
