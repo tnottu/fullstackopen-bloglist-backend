81243f09b85195bd4b43cf13ecabd93e46e59c99
Tomi <tomi.taskila@tyomaa.com>
2021-08-05 15:05:31 +0300

    4.2 Blog list, step2 - Extract Blog-model

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
