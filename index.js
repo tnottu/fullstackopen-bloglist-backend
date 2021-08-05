9b3ce21e58a0c6024c51093e1e01f8c9058805b7
Tomi <tomi.taskila@tyomaa.com>
2021-08-05 15:14:31 +0300

    4.2 Blog list, step2 - Extract app

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const config = require('./utils/config')
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
