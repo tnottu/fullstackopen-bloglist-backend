d3916e63317f0ab63c8eace04d265051b0433fb1
Tomi <tomi.taskila@tyomaa.com>
2021-08-06 18:34:30 +0300

    4.20*: bloglist expansion, step8

--- utils/middleware.js
+++ utils/middleware.js
@@ -32,8 +32,18 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const token = authorization.substring(7)
+    request.token = token
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor,
 }
